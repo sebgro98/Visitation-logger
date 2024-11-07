3f8012a7245ba5a8223c653235aeb0e646a32ae2
sebgro98 <sebgro@kth.se>
2024-11-07 12:16:47 +0100

    updated country name to Required same with node name

--- Backend/ResourceServer/ResourceServer/Model/Node.cs
+++ Backend/ResourceServer/ResourceServer/Model/Node.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AuthenticationServer.Model
 {
@@ -9,6 +10,7 @@ namespace AuthenticationServer.Model
         public Guid Id { get; set; }
 
         [Column("node_name")]
+        [Required]
         public string NodeName { get; set; }
 
     }
