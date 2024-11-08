12ef1737feacfa11c714759b72ef61e43d052831
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-11-08 11:01:29 +0100

    Enhance Login component to differentiate between admin and visitor modes

--- Frontend/Visitation-logger-frontend/src/pages/login/index.jsx
+++ Frontend/Visitation-logger-frontend/src/pages/login/index.jsx
@@ -1,5 +1,18 @@
-const Login = () => {
-  return <div>Loggin sida</div>;
+import PropTypes from "prop-types";
+
+const Login = ({ isAdminMode }) => {
+  return (
+    <>
+      {isAdminMode ? (
+        <div>Admin loggin sida</div>
+      ) : (
+        <div>Visitor loggin sida</div>
+      )}
+    </>
+  );
+};
+Login.propTypes = {
+  isAdminMode: PropTypes.bool.isRequired,
 };
 
 export default Login;
