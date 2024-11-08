d9200426d0d4bebe7056b461b8729163e80bc78d
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-11-08 10:52:03 +0100

    Update routing to include RoleSelection page and add login route

--- Frontend/Visitation-logger-frontend/src/App.jsx
+++ Frontend/Visitation-logger-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import RoleSelection from "./pages/roleSelection";
 import Login from "./pages/login";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<RoleSelection />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
