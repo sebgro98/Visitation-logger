e1b20891ab1b16dd8c011d3ef110ceb8d952e6ca
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-11-11 11:09:28 +0100

    Add Header component and integrate it into the main App layout

--- Frontend/Visitation-logger-frontend/src/App.jsx
+++ Frontend/Visitation-logger-frontend/src/App.jsx
@@ -2,14 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import RoleSelection from "./pages/roleSelection";
 import Login from "./pages/login";
+import Header from "./components/header";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<RoleSelection />} />
-      <Route path="/login/admin" element={<Login isAdminMode={true} />} />
-      <Route path="/login/visitor" element={<Login isAdminMode={false} />} />
-    </Routes>
+    <>
+      <header>
+        <Header isLoggedIn={false} onLogout={() => {}} />
+      </header>
+
+      <Routes>
+        <Route path="/" element={<RoleSelection />} />
+        <Route path="/login/admin" element={<Login isAdminMode={true} />} />
+        <Route path="/login/visitor" element={<Login isAdminMode={false} />} />
+      </Routes>
+    </>
   );
 }
 
