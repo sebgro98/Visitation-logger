952e88e89dff4a9460d3322434a78b32e15a2451
Sebastian <sebgro@kth.se>
2024-11-12 08:42:14 +0100

    Created loginController

---
+++ Backend/ResourceServer/ResourceServer/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResourceServer.Repositories;
+using ResourceServer.Model;
+using ResourceServer.DTO;
+namespace ResourceServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoginController
+    {
+        [HttpPost]
+    }
+}
