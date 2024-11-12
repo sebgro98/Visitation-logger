1a17b861b539bbaa5c3342c70c61d85dfdf0545a
Fredrik Haupt <fredrik.haupt@se.experis.com>
2024-11-12 16:35:01 +0100

    Saving latest workin changes

---
+++ Backend/AuthenticationServer/AuthenticationServer/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        public LoginController()
+        {
+
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "LoginController test get";
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Get([FromBody] object obj)
+        {
+            string objString = obj.ToString();
+            return Ok("LoginController test get " + objString);
+        }
+    }
+}
