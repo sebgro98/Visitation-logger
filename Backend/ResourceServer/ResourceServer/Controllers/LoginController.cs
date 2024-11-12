1d8254f9d87d286d780ebfcd0dfd8bba1647632a
Fredrik Haupt <fredrik.haupt@se.experis.com>
2024-11-12 11:55:57 +0100

    Commit before switching branch

--- Backend/ResourceServer/ResourceServer/Controllers/LoginController.cs
+++ Backend/ResourceServer/ResourceServer/Controllers/LoginController.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using ResourceServer.Repositories;
-using ResourceServer.Model;
-using ResourceServer.DTO;
+
 namespace ResourceServer.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class LoginController
+    public class LoginController : ControllerBase
     {
+        private readonly HttpClient _httpClient;
+        
+        public LoginController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         [HttpPost]
+        public async Task<ActionResult<object>> Login([FromBody] dynamic obj)
+        {
+            // Define the URL of the authentication server
+            var authServerUrl = "https://localhost:[auth server port number]/login";
+
+            // Send the POST request
+            var response = await _httpClient.PostAsync(authServerUrl, obj);
+
+            return Ok(obj);
+        }
     }
 }
