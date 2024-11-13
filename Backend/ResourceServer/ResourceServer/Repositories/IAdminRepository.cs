3ab4d36c1662b88ba69b9da736abd73d3b9f165f
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-11-13 13:49:12 +0100

    Added update and delete endpoints for an admin to handle another admin

--- Backend/ResourceServer/ResourceServer/Repositories/IAdminRepository.cs
+++ Backend/ResourceServer/ResourceServer/Repositories/IAdminRepository.cs
@@ -1,14 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using SharedModels.Models;
+using ResourceServer.DTO;
 
 namespace ResourceServer.Repositories{
 	public interface IAdminRepository{
 		Task<IEnumerable<Admin>> GetAll();
 		Task<Admin> GetById(Guid id);
 		Task<Admin> Insert(Admin obj);
-		Task<Admin> Update(Admin obj);
-		Task<Admin> Delete(object id);
+		Task<Admin> Update(Guid id, AdminDTO dto);
+		Task<bool> Delete(Guid id);
         Task<int> SaveChangesAsync();
-        DbSet<Admin> Table { get; }
+        // DbSet<Admin> Table { get; }
 	}
 }
