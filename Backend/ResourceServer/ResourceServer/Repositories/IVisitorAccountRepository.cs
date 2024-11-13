64896c0ff22021770e7ab8cb22a6c7a5cb9bcd68
Sebastian <sebgro@kth.se>
2024-11-13 16:14:51 +0100

    Done with the updated put on visitor account will now link to it

--- Backend/ResourceServer/ResourceServer/Repositories/IVisitorAccountRepository.cs
+++ Backend/ResourceServer/ResourceServer/Repositories/IVisitorAccountRepository.cs
@@ -6,7 +6,7 @@ namespace ResourceServer.Repositories
     public interface IVisitorAccountRepository
     {
         Task<IEnumerable<VisitorAccount>> GetAllVisitorAccounts();
-        Task<VisitorAccount> GetVisitorAccountById(int id);
+        Task<VisitorAccount> GetVisitorAccountById(Guid id);
         Task<VisitorAccount> UpdateVisitorAccount(Guid id, VisitorAccountDto visitorAccountDto);
         Task<VisitorAccount> CreateVisitorAccount(VisitorAccount visitorAccount);
         Task DeleteVisitorAccount(int id);
