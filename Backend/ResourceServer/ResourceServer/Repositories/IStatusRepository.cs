1d5bcf3ecbb2732a7145e0dd1e1d97707f234e34
sebgro98 <sebgro@kth.se>
2024-11-14 15:44:47 +0100

    Status Repository and DTO done

---
+++ Backend/ResourceServer/ResourceServer/Repositories/IStatusRepository.cs
@@ -0,0 +1,14 @@
+using ResourceServer.DTO;
+using SharedModels.Models;
+
+namespace ResourceServer.Repositories
+{
+    public interface IStatusRepository
+    {
+        Task<IEnumerable<Status>> GetAllStatuses();
+        Task<Status> GetStatusById(Guid id);
+        Task<Status> UpdateStatus(Guid id, StatusDTO statusDTO);
+        Task<Status> CreateStatus(StatusDTO statusDTO);
+        Task DeleteStatus(Guid id);
+    }
+}
