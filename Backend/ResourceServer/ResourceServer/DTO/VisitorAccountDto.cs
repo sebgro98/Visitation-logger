92ef3853c648905b6e6112782a27b9f1ef0a6721
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-11-15 11:04:46 +0100

    Admin and visitor account have default values

--- Backend/ResourceServer/ResourceServer/DTO/VisitorAccountDto.cs
+++ Backend/ResourceServer/ResourceServer/DTO/VisitorAccountDto.cs
@@ -1,28 +1,31 @@
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
+using System.ComponentModel;
 
 namespace ResourceServer.DTO
 {
     public class VisitorAccountDto
     {
-
         [Required]
         [StringLength(100, MinimumLength = 4)]
+        [DefaultValue("visitor")]
         public string UserName { get; set; }
 
         [StringLength(100, MinimumLength = 8)]
         [Required]
+        [DefaultValue("password")]
         public string Password { get; set; }
 
         public DateTime StartDate { get; set; }
 
         public DateTime EndDate { get; set; }
 
+        [DefaultValue("a98dc435-e2ce-4470-be62-bf58f677aec7")] //Service
         public Guid PurposeTypeId { get; set; }
 
+        [DefaultValue("a02cecf6-0749-48ba-bccd-f5871df211ab")] //Jane Smith
         public Guid? VisitorId { get; set; }
 
+        [DefaultValue("28633ad3-cce7-4cd5-85dd-5b27d93a60c9")] //Visitor account
         public Guid AccountTypeId { get; set; }
     }
 }
