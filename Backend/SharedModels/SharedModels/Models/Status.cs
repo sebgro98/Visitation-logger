b7e09319ba340622ea5657db4308ae05d0aa746f
Sebastian <sebgro@kth.se>
2024-11-18 10:26:32 +0100

    models

--- Backend/SharedModels/SharedModels/Models/Status.cs
+++ Backend/SharedModels/SharedModels/Models/Status.cs
@@ -12,24 +12,25 @@ namespace SharedModels.Models
         public Guid Id { get; set; }
 
         // Foreign key for Visitor
-        [JsonIgnore]
+        
         [Column("visitor_id")]
         public Guid VisitorId { get; set; }
 
         // Navigation property for Visitor
+        [JsonIgnore]
         public virtual Visitor Visitor { get; set; }
 
         [Column("check_in_time")]
         public DateTime CheckInTime { get; set; }
 
         [Column("check_in_sign")]
-        public string CheckInSign { get; set; }
+        public string? CheckInSign { get; set; }
 
         [Column("check_out_time")]
         public DateTime? CheckOutTime { get; set; }
 
         [Column("check_out_sign")]
-        public string CheckOutSign { get; set; }
+        public string? CheckOutSign { get; set; }
 
         [Column("last_export_date")]
         public DateTime? LastExportDate { get; set; }
