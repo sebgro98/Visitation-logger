aabd0dccb37bdeb316cfdf2e61bebc34b6423035
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-11-19 16:17:11 +0100

    Add API functions for nodes, account types, and admin account creation

--- Frontend/Visitation-logger-frontend/src/services/apiClient.js
+++ Frontend/Visitation-logger-frontend/src/services/apiClient.js
@@ -12,6 +12,18 @@ async function getAllAdminAccounts() {
   return await get("Admin");
 }
 
+async function getAllNodes() {
+  return await get("Node");
+}
+
+async function getAllAccountTypes() {
+  return await get("AccountType");
+}
+
+async function createAdminAccount(account) {
+  return await post("Admin", account);
+}
+
 async function post(endpoint, data, auth = true) {
   return await request("POST", endpoint, data, auth);
 }
@@ -36,6 +48,10 @@ async function request(method, endpoint, data, auth = true) {
     opts.headers["Authorization"] = `Bearer ${localStorage.getItem("token")}`;
   }
 
+  console.log("Requesting", endpoint, opts);
+  console.log("Data", data);
+  console.log("Auth", auth);
+
   const response = await fetch(`${API_URL}/${endpoint}`, opts);
 
   if (!response.ok) {
@@ -45,4 +61,11 @@ async function request(method, endpoint, data, auth = true) {
   return response.json();
 }
 
-export { login, getAllVisitorAccounts, getAllAdminAccounts };
+export {
+  login,
+  getAllVisitorAccounts,
+  getAllAdminAccounts,
+  getAllNodes,
+  getAllAccountTypes,
+  createAdminAccount,
+};
