2422dc57b7b9e17532b0fe750074e4fcc37cce1f
sebgro98 <sebgro@kth.se>
2024-11-20 10:59:33 +0100

    update so that the message is in json and that the correct status code is being sent

--- Backend/AuthenticationServer/AuthenticationServer/Controllers/LoginController.cs
+++ Backend/AuthenticationServer/AuthenticationServer/Controllers/LoginController.cs
@@ -27,15 +27,16 @@ namespace AuthenticationServer.Controllers
             var token = await _authService.AuthenticateAsync(dto);
             if (token == null)
             {
-                return Unauthorized("Password or Username incorrect");
+                return Unauthorized("Password or Username incorrect" );
             }
 
-            if(token == "Account has expiered")
+            if (token == "Account has expired")
             {
-                return Unauthorized("Account has expiered");
+                return Unauthorized("Account has expired" );
             }
 
             return Ok(token);
+
         }
 
 
