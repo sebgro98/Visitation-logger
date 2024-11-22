4be1ae5bd4e724cb7b586037f70f5ef3ea5c950b
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-11-22 15:19:18 +0100

    Add EditAccount component for managing account edits with visitor and admin modes

---
+++ Frontend/Visitation-logger-frontend/src/pages/editAccount/index.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState, useMemo } from "react";
+import PropTypes from "prop-types";
+import HandleAccount from "../../components/handleAccount";
+import {
+  getAdminAccountById,
+  getVisitorAccountById,
+  updateAdminAccount,
+  updateVisitorAccount,
+} from "../../services/apiClient";
+import { useParams } from "react-router-dom";
+import LoadingCircle from "../../components/loadingCircle";
+
+const EditAccount = ({ isEditVisitorMode }) => {
+  const [fields, setFields] = useState({});
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (isEditVisitorMode) {
+          const visitorAccount = await getVisitorAccountById(id);
+          setFields(visitorAccount);
+        } else {
+          const adminAccount = await getAdminAccountById(id);
+          setFields({
+            id: adminAccount.id,
+            username: adminAccount.username,
+            fullName: adminAccount.fullName,
+            accountTypeId: adminAccount.accountType.id,
+            nodeId: adminAccount.node.id,
+          });
+        }
+      } catch (error) {
+        console.error("Error fetching account data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [isEditVisitorMode, id]);
+
+  const memoizedFields = useMemo(() => fields, [fields]);
+
+  if (loading) {
+    return <LoadingCircle />;
+  }
+
+  return (
+    <HandleAccount
+      accountType={isEditVisitorMode ? "visitor" : "admin"}
+      handleAccountAction={
+        isEditVisitorMode ? updateVisitorAccount : updateAdminAccount
+      }
+      fields={memoizedFields}
+      isEditMode={true}
+    />
+  );
+};
+
+EditAccount.propTypes = {
+  isEditVisitorMode: PropTypes.bool.isRequired,
+};
+
+export default EditAccount;
